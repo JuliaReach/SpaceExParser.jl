1051d2c7bf198a0612e39dbab276f9ab0a21ec09
Marcelo Forets <mforets@gmail.com>
2018-08-28 15:31:22 -0300

    Update unit_affine.jl
    fix indentation

--- test/unit_affine.jl
+++ test/unit_affine.jl
@@ -16,10 +16,10 @@ AFFINE_SYSTEM = ConstrainedLinearControlContinuousSystem
     @test S.X[1] isa HalfSpace && S.X[1].a == [-1.0, 0.0] && S.X[1].b == 0.0 # -x <= 0 <=> x >= 0
     @test S.U[1] isa Singleton && S.U[1].element == [1.0]
 
-	R = H.resetmaps[1]
-	@test R.A == [1.0 0.0; 0.0 -0.75]
-	@test isempty(R.B) && size(R.B) == (2, 0)
-	@test length(R.X) == 2
-	@test R.X[1] isa Hyperplane && R.X[1].a == [1.0, 0.0] && R.X[1].b == 0.0 # x = 0
-	@test R.X[2] isa HalfSpace && R.X[2].a == [0.0, 1.0] && R.X[1].b == 0.0 # v < 0
+    R = H.resetmaps[1]
+    @test R.A == [1.0 0.0; 0.0 -0.75]
+    @test isempty(R.B) && size(R.B) == (2, 0)
+    @test length(R.X) == 2
+    @test R.X[1] isa Hyperplane && R.X[1].a == [1.0, 0.0] && R.X[1].b == 0.0 # x = 0
+    @test R.X[2] isa HalfSpace && R.X[2].a == [0.0, 1.0] && R.X[1].b == 0.0 # v < 0
 end
