5653f2857973ae74061f65f645a68827209f006a
Unknown <kostemkin@gmail.com>
2018-09-30 00:46:28 +1000

    updated initial set

--- test/runDriveTrain.jl
+++ test/runDriveTrain.jl
@@ -2,10 +2,8 @@
 # ============================
 
 using HybridSystems, MathematicalSystems, LazySets, Plots
+import SX:is_halfspace, is_hyperplane, convert, parse_sxmath
 
-AFFINE_SYSTEM = ConstrainedLinearControlContinuousSystem
-HS = readsxmodel("examples/Drivetrain/drivetrain_theta1_5percent_flat_manually.xml", ST=AFFINE_SYSTEM)
-# initial condition in mode 1
 
 x1 = parse_sxmath("x1 - -0.0488 == 0.0005600000000000001 * (x4 - 20.0)")
 x2 = parse_sxmath("x2 - -15.67 == 0.46699999999999997 * (x4 - 20.0)")
@@ -17,16 +15,23 @@ x7 = parse_sxmath("x7 - 240.0 == 12.0 * (x4 - 20.0)")
 x8 = parse_sxmath("x8 - -0.0019199999999999998 == 0.00005999999999999998 * (x4 - 20.0)")
 x9 = parse_sxmath("x9 - 20.0 == 1.0 * (x4 - 20.0)")
 t = parse_sxmath("t == 0.0")
+
+expr_p = parse_sxmath("x1 - -0.0488 == 0.0005600000000000001 * (x4 - 20.0) & x2 - -15.67 == 0.46699999999999997 * (x4 - 20.0) & x3 == 0.0 & x5 == 0.0 & x6 - 20.0 == 1.0 * (x4 - 20.0) & x7 - 240.0 == 12.0 * (x4 - 20.0) & x8 - -0.0019199999999999998 == 0.00005999999999999998 * (x4 - 20.0) & x9 - 20.0 == 1.0 * (x4 - 20.0) & 20.0 <= x4 & x4 <= 40.0 & t == 0.0")
 expr = [x1, x2, x3, x4,x5,x6,x7,x8,x9,t]
 x0sets = []
 vars = Basic[:x1, :x2, :x3, :x4, :x6, :x7, :x8, :x9, :t]
-for expr_i in expr
+for expr_i in expr_p
     if is_halfspace(expr_i)
         push!(x0sets, convert(HalfSpace, expr_i, vars=vars))
     elseif is_hyperplane(expr_i)
         push!(x0sets, convert(Hyperplane, expr_i, vars=vars))
     end
 end
+
+
+AFFINE_SYSTEM = ConstrainedLinearControlContinuousSystem
+HS = readsxmodel("examples/Drivetrain/drivetrain_theta1_5percent_flat_manually.xml", ST=AFFINE_SYSTEM)
+# initial condition in mode 1
 
 # calculate reachable states up to time T
 prob = InitialValueProblem(HS, x0sets)
