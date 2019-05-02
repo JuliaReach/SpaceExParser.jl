6ffd5f2d501e52051638e4e020a18ed93c17d1fc
mforets <mforets@gmail.com>
2019-05-02 17:18:28 +1000

    add get_coeffs function

--- src/symbolic.jl
+++ src/symbolic.jl
@@ -1,3 +1,5 @@
+export get_coeffs
+
 """
 linearHS(HDict; ST=ConstrainedLinearControlContinuousSystem,
                 STD=ConstrainedLinearControlDiscreteSystem,
@@ -48,6 +50,7 @@ function linearHS(HDict; ST=ConstrainedLinearControlContinuousSystem,
     state_variables = Vector{Basic}()
     input_variables = Vector{Basic}()
 
+    # in this point it is defined which variables are "state" and which are "input"
     for vi in keys(variables)
        if variables[vi]["controlled"]
            push!(state_variables, convert(Basic, vi))
@@ -243,4 +246,32 @@ function linearHS(HDict; ST=ConstrainedLinearControlContinuousSystem,
 
 
     return (modes, resetmaps)
+end
+
+# x' = Ax + Bu + c
+function get_coeffs(flow, n, m, state_vars, input_vars)
+    
+    A = Matrix{Float64}(undef, n, n)
+    B = Matrix{Float64}(undef, n, m)
+    c = zeros(Float64, n)
+
+    for (i, fi) in enumerate(flow)
+        #println(fi)
+        RHS = convert(Basic, fi.args[2])
+        #println(RHS)
+        #println()
+        const_term = subs(RHS, [xi=>0.0 for xi in state_vars]...,
+                               [ui=>0.0 for ui in input_vars]...)
+
+        if const_term != 0.0
+            C[i] = const_term
+            isaffine = true
+        end
+        # terms linear in the *state* variables
+        A[i, :] = convert.(Float64, diff.(RHS, state_vars))
+
+        # terms linear in the *input* variables
+        B[i, :] = convert.(Float64, diff.(RHS, input_vars))
+    end
+    return A, B, c
 end
