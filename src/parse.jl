de5802c518ebf4cc73c1327d87d38b7b034c3b46
schillic <schillic@informatik.uni-freiburg.de>
2019-08-12 20:51:52 +0200

    fix doctests and package imports

--- src/parse.jl
+++ src/parse.jl
@@ -51,7 +51,7 @@ Vector of expressions, equations or inequalities.
 ### Examples
 
 ```jldoctest parse_sxmath
-julia> import SX.parse_sxmath
+julia> using SX: parse_sxmath
 
 julia> parse_sxmath("x >= 0")
 1-element Array{Expr,1}:
