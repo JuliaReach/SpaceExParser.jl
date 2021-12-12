5aa0d50e72442c96dcbe2ac7a11039294e213477
schillic <git@christianschilling.net>
2021-12-12 17:08:15 +0100

    simpler style file + activate docs previews

--- docs/make.jl
+++ docs/make.jl
@@ -1,22 +1,24 @@
 using Documenter, SpaceExParser
 
-DocMeta.setdocmeta!(SpaceExParser, :DocTestSetup, :(using SpaceExParser); recursive=true)
+DocMeta.setdocmeta!(SpaceExParser, :DocTestSetup,
+                    :(using SpaceExParser); recursive=true)
 
 makedocs(
     sitename = "SpaceExParser.jl",
     modules = [SpaceExParser],
     format = Documenter.HTML(
         prettyurls = get(ENV, "CI", nothing) == "true",
-        assets = ["assets/juliareach.css"]),
+        assets = ["assets/aligned.css"]),
+    strict = true,
     pages = [
         "Home" => "index.md",
         "Examples" => Any["Introduction" => "examples/examples.md", "Bouncing ball" => "examples/bball.md"],
         "Library" => Any["Types" => "lib/types.md", "Methods" => "lib/methods.md"],
         "About" => "about.md"
-    ],
-    strict = true
+    ]
 )
 
 deploydocs(
-    repo = "github.com/JuliaReach/SpaceExParser.jl.git"
+    repo = "github.com/JuliaReach/SpaceExParser.jl.git",
+    push_preview = true
 )
