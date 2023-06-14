3033b029e800b389c0c231b01272f1e802856917
schillic <git@christianschilling.net>
2023-06-14 06:12:16 +0200

    format docs/make.jl and code

--- docs/make.jl
+++ docs/make.jl
@@ -3,22 +3,16 @@ using Documenter, SpaceExParser
 DocMeta.setdocmeta!(SpaceExParser, :DocTestSetup,
                     :(using SpaceExParser); recursive=true)
 
-makedocs(
-    sitename = "SpaceExParser.jl",
-    modules = [SpaceExParser],
-    format = Documenter.HTML(
-        prettyurls = get(ENV, "CI", nothing) == "true",
-        assets = ["assets/aligned.css"]),
-    strict = true,
-    pages = [
-        "Home" => "index.md",
-        "Examples" => Any["Introduction" => "examples/examples.md", "Bouncing ball" => "examples/bball.md"],
-        "Library" => Any["Types" => "lib/types.md", "Methods" => "lib/methods.md"],
-        "About" => "about.md"
-    ]
-)
+makedocs(; sitename="SpaceExParser.jl",
+         modules=[SpaceExParser],
+         format=Documenter.HTML(; prettyurls=get(ENV, "CI", nothing) == "true",
+                                assets=["assets/aligned.css"]),
+         strict=true,
+         pages=["Home" => "index.md",
+                "Examples" => Any["Introduction" => "examples/examples.md",
+                                  "Bouncing ball" => "examples/bball.md"],
+                "Library" => Any["Types" => "lib/types.md", "Methods" => "lib/methods.md"],
+                "About" => "about.md"])
 
-deploydocs(
-    repo = "github.com/JuliaReach/SpaceExParser.jl.git",
-    push_preview = true
-)
+deploydocs(; repo="github.com/JuliaReach/SpaceExParser.jl.git",
+           push_preview=true)
