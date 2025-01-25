2ba9b159f111870431e7a67362ab6764dc334513
schillic <git@christianschilling.net>
2025-01-25 20:22:57 +0100

    use DocumenterCitations for bibliography

--- docs/make.jl
+++ docs/make.jl
@@ -1,17 +1,21 @@
-using Documenter, SpaceExParser
+using Documenter, SpaceExParser, DocumenterCitations
 
 DocMeta.setdocmeta!(SpaceExParser, :DocTestSetup,
                     :(using SpaceExParser); recursive=true)
 
+bib = CitationBibliography(joinpath(@__DIR__, "src", "refs.bib"); style=:alpha)
+
 makedocs(; sitename="SpaceExParser.jl",
          modules=[SpaceExParser],
          format=Documenter.HTML(; prettyurls=get(ENV, "CI", nothing) == "true",
-                                assets=["assets/aligned.css"]),
+                                assets=["assets/aligned.css", "assets/citations.css"]),
          pagesonly=true,
+         plugins=[bib],
          pages=["Home" => "index.md",
                 "Examples" => Any["Introduction" => "examples/examples.md",
                                   "Bouncing ball" => "examples/bball.md"],
                 "Library" => Any["Types" => "lib/types.md", "Methods" => "lib/methods.md"],
+                "Bibliography" => "bibliography.md",
                 "About" => "about.md"])
 
 deploydocs(; repo="github.com/JuliaReach/SpaceExParser.jl.git",
