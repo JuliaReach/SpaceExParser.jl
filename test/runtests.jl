5f499dd1214f065ee2132442490e4066abb809d6
schillic <git@christianschilling.net>
2026-02-22 21:51:07 +0100

    do not run parser tests in v1.12.5 and use v1.11 in CI

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,8 +1,8 @@
 using Test, SpaceExParser, HybridSystems, MathematicalSystems
 
 include("unit_parse.jl")
-@static if VERSION < v"1.12.0" || VERSION > v"1.12.4"
-    # Julia parser is broken for `'` in v1.12.0 - v1.12.4
+@static if VERSION < v"1.12.0" || VERSION > v"1.12.5"
+    # Julia parser is broken for `'` in v1.12.0 - v1.12.5
     include("unit_examples.jl")
     include("unit_affine.jl")
 end
